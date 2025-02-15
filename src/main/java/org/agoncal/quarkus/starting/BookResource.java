cf58d7addb3f7bdab2b3f6829c795bca73881fc4
Pablo Endara-Santiago <litotechenterprise@gmail.com>
2025-02-15 15:41:45 -0500

    completed adding new endpoint

--- src/main/java/org/agoncal/quarkus/starting/BookResource.java
+++ src/main/java/org/agoncal/quarkus/starting/BookResource.java
@@ -2,10 +2,12 @@ package org.agoncal.quarkus.starting;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 import java.util.List;
+import java.util.Optional;
 
 @Path("/api/books")
 @Produces(MediaType.APPLICATION_JSON)
@@ -13,22 +15,23 @@ public class BookResource {
 
     @GET
     public List<Book> getAllBooks() {
-        return List.of(new Book(1, "New home", "Pablo", 2025, "Comedy"),
-                new Book(2, "Booklyn", "Pablo", 2025, "Comedy"),
-                new Book(3, "Monkey mind", "Pablo", 2025, "Comedy"),
-                new Book(4, "Battle Scars", "Pablo", 2025, "Comedy"));
+        return List.of(new Book(1, "New home", "Pablo Endara", 2022, "Comedy"),
+                new Book(2, "Booklyn", "Pablo Endara", 2019, "Sci-fi"),
+                new Book(3, "Monkey mind", "Pablo Endara", 2010, "Rom-Com"),
+                new Book(4, "Battle Scars", "Pablo Endara", 2005, "Comedy"));
     }
 
 
-//    @GET
-//    @Path("{id}")
-//    public Book getBookById() {
-//        return getAllBooks().stream().filter(::)
-//    }
+    @GET
+    @Path("{id}")
+    public Optional<Book> getBookById(@PathParam("id") int id) {
+        return getAllBooks().stream().filter(book -> book.id == id).findFirst();
+    }
 
 
     @GET
     @Path("/count")
+    @Produces(MediaType.TEXT_PLAIN)
     public int getBookCount(){
         return getAllBooks().size();
     }
